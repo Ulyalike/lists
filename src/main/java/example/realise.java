9261b530831a9638062f453cabe1b00961f63314
Ulyalike <ryzhenkova.ue@phystech.edu>
2024-10-11 14:46:54 +0300

    Update realise.java

--- src/main/java/example/realise.java
+++ src/main/java/example/realise.java
@@ -1,6 +1,18 @@
+/**
+ * create interface with different methods which is parameterized 
+ */
 package example;
 public interface realise<A> {
+/**
+ * add elements to the end of the list
+ */
   void add(A element);
+/**
+ * return element by index
+ */
   A get(int index);
+/**
+ * remove element by index
+ */
   void remove(int index);
 }
